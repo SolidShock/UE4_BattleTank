28aca4f0bf4c938129c180225e2827090da5b94e
SolidShock <50242458+SolidShock@users.noreply.github.com>
2020-02-09 12:46:29 +0200

    BT07 Tank possesion!
    Made a player controller and ai controller that check if it's connected with the correct tank and spit out some text in the logs.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,23 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posesing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is posesing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
