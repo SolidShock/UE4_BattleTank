08f50e63fcccb23f47480d45cd91c79ac8fca232
SolidShock <50242458+SolidShock@users.noreply.github.com>
2020-08-14 10:18:47 +0300

    BT18 Added new movement option!
    Now, the player can also move the tank forwards and backwards using W and S, redesigned the movement component to not be directly inherited on the tank blueprint.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,7 +13,6 @@ ATank::ATank()
 	PrimaryActorTick.bCanEverTick = false;
 
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 // Called when the game starts or when spawned
--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
+
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
 }
