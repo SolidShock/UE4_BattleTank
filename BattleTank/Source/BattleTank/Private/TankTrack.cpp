20ab05d77c30ab2b602a7ba5de6fe7db292d5f8a
SolidShock <50242458+SolidShock@users.noreply.github.com>
2020-08-28 09:41:01 +0300

    BT30 No more tank drifting.
    Terrains still a bit buggy, but now tanks can't slide so much around on the flat surface, they got forces that push against them.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -1,7 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TankTrack.h"
+
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -21,4 +21,9 @@ public:
 	// Max force per track in Newtons.
 	UPROPERTY(EditDefaultsOnly)
 	float TrackMaxDrivingForce = 60000000.0f;
+
+private:
+	UTankTrack();
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
