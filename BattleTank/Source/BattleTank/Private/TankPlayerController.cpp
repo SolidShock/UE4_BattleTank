f9047e4c3e31333bda8cfe1b7105c94e4f2fdc86
SolidShock <lopexsw@gmail.com>
2021-01-05 20:49:16 +0200

    BT50 The Near End
    The majority of the project is finished. I never intended to make this project super polished, but that's somewhat of what I'm going to do next. Polish the project a bit, make a new map and I think that'll be it.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -9,6 +9,7 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	if (!GetPawn()) return;
 	auto AimingComponent = GetPawn()->FindComponentByClass<UTankAimingComponent>();
 	if (!ensure(AimingComponent)) { return; }
 	FoundAimingComponent(AimingComponent);
--- BattleTank/Source/BattleTank/Public/SprungWheel.h
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -27,6 +27,11 @@ protected:
 private:
 	void SetupConstraint();
 
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ApplyForce();
+
 	// Components
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USphereComponent* Wheel = nullptr;
@@ -39,4 +44,6 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPhysicsConstraintComponent* AxleWheelConstraint = nullptr;
+
+	float TotalForceMagnitudeThisFrame = 0;
 };
